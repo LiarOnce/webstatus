cb8149d654415a52341205aec1253f0d60c9c5e8
LiarOnce <qq1062787843@hotmail.com>
2023-05-27 12:14:29 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,29 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '网站状态',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur2149808-7086bd354952324b9fef7c26'
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '博客',
+      url: 'https://liaronce.com/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/LiarOnce'
+    }
+  ],
+};
